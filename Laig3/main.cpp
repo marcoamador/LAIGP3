224696a41712638032315184f1ab15c40b776ff0
Pedro Borges <highvolt16@gmail.com>
2011-12-12 22:36:38 +0000

    Initial Commit

---
+++ Laig3/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Laig3
+//
+//  Created by Pedro Borges on 12/12/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
